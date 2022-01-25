428c9f43b945b050666afe3bc774bbcceda5725a
Latokike <81393451+Latokike@users.noreply.github.com>
2022-01-25 10:47:23 -0800

    Delete main directory

--- main/java/com/github/latokike/registry/ModDamageSources.java
+++
@@ -1,9 +0,0 @@
-package com.github.latokike.registry;
-
-import io.github.apace100.calio.mixin.DamageSourceAccessor;
-import net.minecraft.entity.damage.DamageSource;
-
-public class ModDamageSources {
-
-    public static final DamageSource BOIL_IN_WATER = ((DamageSourceAccessor)((DamageSourceAccessor)DamageSourceAccessor.createDamageSource("boil_in_water")).callSetBypassesArmor()).callSetUnblockable();
-}
